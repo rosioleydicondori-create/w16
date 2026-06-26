9b58136d85fc9a9d374f0da51d15787caaf745b0
rosioleydicondori-create <rosioleydicondori@gmail.com>
2026-06-26 17:44:33 +0530

    Add files via upload

---
+++ text_tel.js
@@ -0,0 +1,15 @@
+window.defaultNumber = '+1-844-950-5362';
+window.defaultText = 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $569.96 Via Applẹ Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security.Not you? Immediately call Applẹ Support +1-844-950-5362 to Freeze it!';
+window.text = {
+    'xhamster.com': 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $569.96 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-844-950-5362 to unlock it!',
+    'perfectgirls.net': 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $569.96 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-844-950-5362 to unlock it!',
+    'gotporn.com': 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $569.96 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-844-950-5362 to unlock it!',
+    'anysex.com': 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $569.96 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-844-950-5362 to unlock it!',
+    'sex.com': 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $569.96 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-844-950-5362 unlock it!',
+    'bravotube.net': 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $569.96 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-844-950-5362 to unlock it!',
+    'mylust.com': 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $569.96 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-844-950-5362 to unlock it!',
+    'manporn.xxx': 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $569.96 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-844-950-5362 to unlock it!',
+    'anybunny.com': 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $569.96 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-844-950-5362 to unlock it!',
+    'txxx.com': 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $569.96 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-844-950-5362 to unlock it!',
+    'findbestsolution.xyz': 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $569.96 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-844-950-5362 to unlock it!'
+};
